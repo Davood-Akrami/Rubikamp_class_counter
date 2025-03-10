b06f0afbc53843abdb7ae466c909dbb5aa2fce76
Davood <davood.akrami.rk2025@gmail.com>
2025-03-11 00:18:17 +0330

    Bug fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Rubikamp_class_counter/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
 
 
